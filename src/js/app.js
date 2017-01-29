ccd4fa519f2632b64fddf731fbdd8819ab33265a
Валера Любимов <vudav@MacBook-Pro-Valera.local>
2017-01-29 18:54:08 +0300

    Page header done

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,7 @@
 // add this some vendor js scripts for this page (use gulp-rigger syntax)
 //= '../../bower_components/jquery/dist/jquery.js'
+//= '../../bower_components/bootstrap/js/modal.js'
+//= '../../bower_components/bootstrap/js/dropdown.js'
 
 $(document).ready(function() {
 
