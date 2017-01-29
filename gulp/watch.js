b0af63f4eb02b770586c9da9d80f8508c79b9d17
Валера Любимов <vudav@MacBook-Pro-Valera.local>
2017-01-29 17:28:52 +0300

    Fixed gulp

--- gulp/watch.js
+++ gulp/watch.js
@@ -7,10 +7,8 @@ gulp.task('watch', ['webserver'],function() {
 	gulp.watch(config.pathTo.Watch.Jade, ['jade']);
 	gulp.watch(config.pathTo.Watch.Images, ['images']);
 	gulp.watch(config.pathTo.Watch.PngSprite, ['png-sprite']);
-	gulp.watch(config.pathTo.Watch.SvgSprite, ['svg-sprite']);
 	gulp.watch('bower.json', ['bower']);
 	gulp.watch(config.pathTo.Watch.Txt, ['txt']);
 	gulp.watch(config.pathTo.Watch.JS, ['js']);
 	gulp.watch(config.pathTo.Watch.Fonts, ['fonts']);
-	gulp.watch(config.pathTo.Watch.Favicon, ['generate-favicon','inject-favicon-markups']);
 });
---
+++ src/js/index-script.js
@@ -0,0 +1,9 @@
+// add this some vendor js scripts for this page (use gulp-rigger syntax)
+
+jQuery(document).ready(function(){
+
+	app.init = function(){};
+
+	app.init();
+
+});
